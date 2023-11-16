000bfa5538bf7f2feed9b7b8dde56f0aff1460de
MinHyeong <p610222@naver.com>
2023-11-16 20:05:42 +0900

    배경 및 애벌레 그림 완성
    추가

--- VP_Fianl_Project/VP_Fianl_Project/Game.cpp
+++ VP_Fianl_Project/VP_Fianl_Project/Game.cpp
@@ -1,5 +1,8 @@
+#pragma comment(lib, "msimg32.lib")
 #include <windows.h>
 #include <TCHAR.H>
+#define _USE_MATH_DEFINES
+#include <math.h>
 #include "resource.h"
 
 LRESULT CALLBACK WndProc(HWND hwnd, UINT iMsg, WPARAM wParam, LPARAM lParam);
@@ -44,11 +47,13 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdLi
 	return (int)msg.wParam;
 }
 
-
-
 #define ITEMNUM 10
 
 HBITMAP appleBitmap;
+HBITMAP HeadBitmap;
+HBITMAP BodyBitmap;
+HBITMAP TailBitmap;
+HBITMAP BackGroundBitmap;
 int board[22][22];	
 int wormX[100], wormY[100];
 int xDirect, yDirect, len;
@@ -85,8 +90,143 @@ void ItemReload() {
 	}
 }
 
+HBITMAP GetRotatedBitmap(HDC hdc, HBITMAP hBitmap, float radians, COLORREF clrBack)
+{
+	HDC sourceDC, destDC;
+	BITMAP bm;
+
+	HBITMAP hbmResult;
+	HBITMAP hbmOldSource;
+	HBITMAP hbmOldDest;
+	HBRUSH hbrBack;
+	HBRUSH hbrOld;
+
+	XFORM xform;
+
+	float cosine, sine;
+	int x1, y1, x2, y2, x3, y3, minx, miny, maxx, maxy, w, h;
+
+
+	sourceDC = CreateCompatibleDC(hdc);
+	destDC = CreateCompatibleDC(hdc);
+
+	GetObject(hBitmap, sizeof(bm), &bm);
+
+	cosine = cos(radians);
+	sine = sin(radians);
+
+	x1 = (int)(bm.bmHeight * sine);
+	y1 = (int)(bm.bmHeight * cosine);
+	x2 = (int)(bm.bmWidth * cosine + bm.bmHeight * sine);
+	y2 = (int)(bm.bmHeight * cosine - bm.bmWidth * sine);
+	x3 = (int)(bm.bmWidth * cosine);
+	y3 = (int)(-bm.bmWidth * sine);
+
+	minx = min(0, min(x1, min(x2, x3)));
+	miny = min(0, min(y1, min(y2, y3)));
+	maxx = max(0, max(x1, max(x2, x3)));
+	maxy = max(0, max(y1, max(y2, y3)));
+
+	w = maxx - minx;
+	h = maxy - miny;
+
+	hbmResult = CreateCompatibleBitmap(hdc, w, h);
+
+	hbmOldSource = (HBITMAP)SelectObject(sourceDC, hBitmap);
+	hbmOldDest = (HBITMAP)SelectObject(destDC, hbmResult);
+
+	hbrBack = CreateSolidBrush(clrBack);
+	hbrOld = (HBRUSH)SelectObject(destDC, hbrBack);
+	PatBlt(destDC, 0, 0, w, h, PATCOPY);
+	DeleteObject(SelectObject(destDC, hbrOld));
+
+	SetGraphicsMode(destDC, GM_ADVANCED);
+	xform.eM11 = cosine;
+	xform.eM12 = -sine;
+	xform.eM21 = sine;
+	xform.eM22 = cosine;
+	xform.eDx = (float)-minx;
+	xform.eDy = (float)-miny;
+
+	SetWorldTransform(destDC, &xform);
+
+	BitBlt(destDC, 0, 0, bm.bmWidth, bm.bmHeight, sourceDC, 0, 0, SRCCOPY);
+
+	SelectObject(sourceDC, hbmOldSource);
+	SelectObject(destDC, hbmOldDest);
+
+	DeleteDC(sourceDC);
+	DeleteDC(destDC);
+
+	return hbmResult;
+}
+
+void DrawApple(HDC hdc, int x, int y) {
+	HDC MemDC;
+	HBITMAP oldBitmap;
+	BITMAP bit;
+	int bx, by;
+
+	MemDC = CreateCompatibleDC(hdc);
+	oldBitmap = (HBITMAP)SelectObject(MemDC, appleBitmap);
+	
+
+	GetObject(appleBitmap, sizeof(bit), &bit);
+	bx = bit.bmWidth;
+	by = bit.bmHeight;
+
+	TransparentBlt(hdc, x * 20, y * 20, bx, by, MemDC, 0, 0, bx, by, RGB(255, 255, 255));
+
+	SelectObject(MemDC, oldBitmap);
+	DeleteDC(MemDC);
+}
+
+void DrawLarva(HDC hdc, HBITMAP larvaBitmap, int index){
+	HDC MemDC;
+	HBITMAP oldBitmap;
+	BITMAP bit;
+	int bx, by;
+
+	MemDC = CreateCompatibleDC(hdc);
+	oldBitmap = (HBITMAP)SelectObject(MemDC, larvaBitmap);
+
+	GetObject(larvaBitmap, sizeof(bit), &bit);
+	bx = bit.bmWidth;
+	by = bit.bmHeight;
+
+	TransparentBlt(hdc, wormX[index] * 20, wormY[index] * 20, bx, by, MemDC, 0, 0, bx, by, RGB(255, 255, 255));
+	//BitBlt(hdc, wormX[index] * 20, wormY[index] * 20, (wormX[index] + 1) * 20, (wormY[index] + 1) * 20, MemDC, 0, 0, SRCCOPY);
+
+	SelectObject(MemDC, oldBitmap);
+	DeleteDC(MemDC);
+
+	SelectObject(hdc, CreatePen(PS_SOLID, 2, RGB(0, 0, 255)));
+}
+
+void DrawBackGround(HDC hdc) {
+	HDC MemDC;
+	HBITMAP oldBitmap;
+	BITMAP bit;
+	int bx, by;
+
+	MemDC = CreateCompatibleDC(hdc);
+	oldBitmap = (HBITMAP)SelectObject(MemDC, BackGroundBitmap);
+
+	GetObject(BackGroundBitmap, sizeof(bit), &bit);
+	bx = bit.bmWidth;
+	by = bit.bmHeight;
+
+	BitBlt(hdc, 0, 0, 440, 440, MemDC, 0, 0, SRCCOPY);
+
+	SelectObject(MemDC, oldBitmap);
+	DeleteDC(MemDC);
+
+	SelectObject(hdc, CreatePen(PS_SOLID, 2, RGB(0, 0, 255)));
+}
+
 void DrawGameBoard(HDC hdc)
 {
+	DrawBackGround(hdc);
 	int i, x, y;
 	for (y = 0; y < 22; y++) {
 		for (x = 0; x < 22; x++)
@@ -97,25 +237,7 @@ void DrawGameBoard(HDC hdc)
 				Rectangle(hdc, x * 20, y * 20, (x + 1) * 20, (y + 1) * 20);
 				break;
 			case 2:
-				//SelectObject(hdc, GetStockObject(BLACK_BRUSH));
-				//Ellipse(hdc, x * 20, y * 20, (x + 1) * 20, (y + 1) * 20);
-				//SelectObject(hdc, GetStockObject(WHITE_BRUSH));
-				HDC MemDC;
-				HBITMAP oldBitmap;
-				BITMAP bit;
-				int bx, by;
-
-				MemDC = CreateCompatibleDC(hdc);
-				oldBitmap = (HBITMAP)SelectObject(MemDC, appleBitmap);
-
-				GetObject(appleBitmap, sizeof(bit), &bit);
-				bx = bit.bmWidth;
-				by = bit.bmHeight;
-
-				BitBlt(hdc, x * 20, y * 20, x * 20 + bx, y * 20 + by, MemDC, 0, 0, SRCCOPY);
-
-				SelectObject(MemDC, oldBitmap);
-				DeleteDC(MemDC);
+				DrawApple(hdc, x, y);
 				break;
 			case 4:
 				SelectObject(hdc, GetStockObject(GRAY_BRUSH));
@@ -126,13 +248,14 @@ void DrawGameBoard(HDC hdc)
 		}
 	}
 
-	SelectObject(hdc, CreatePen(PS_SOLID, 2, RGB(255, 0, 0)));
-	Ellipse(hdc, wormX[0] * 20, wormY[0] * 20, (wormX[0] + 1) * 20, (wormY[0] + 1) * 20);
-	SelectObject(hdc, CreatePen(PS_SOLID, 2, RGB(0, 0, 255)));
-
-	for (i = 1; i < len; i++)
-		Ellipse(hdc, wormX[i] * 20, wormY[i] * 20, (wormX[i] + 1) * 20, (wormY[i] + 1) * 20);
-
+	DrawLarva(hdc, HeadBitmap, 0);
+	for (i = 1; i < len; i++) {
+		if(i == len - 1)
+			DrawLarva(hdc, TailBitmap, i);
+		else
+			DrawLarva(hdc, BodyBitmap, i);
+	}
+		
 	TCHAR scoreText[20];
 	wsprintf(scoreText, _T("score : %d"), score);
 	TextOut(hdc, 0, 22 * 20, scoreText, _tcslen(scoreText));
@@ -141,6 +264,10 @@ void DrawGameBoard(HDC hdc)
 void GameInit()
 {
 	appleBitmap = LoadBitmap(g_hlnst, MAKEINTRESOURCE(IDB_BITMAP1));
+	HeadBitmap = LoadBitmap(g_hlnst, MAKEINTRESOURCE(IDB_BITMAP3));
+	BodyBitmap = LoadBitmap(g_hlnst, MAKEINTRESOURCE(IDB_BITMAP4));
+	TailBitmap = LoadBitmap(g_hlnst, MAKEINTRESOURCE(IDB_BITMAP5));
+	BackGroundBitmap = LoadBitmap(g_hlnst, MAKEINTRESOURCE(IDB_BITMAP6));
 	int i;
 	for (i = 0; i < 22; i++)
 	{
@@ -191,41 +318,73 @@ void GameOver(HWND hwnd) {
 	}
 }
 
-void DirectControl(int DirectKey)
+void DirectControl(int DirectKey, HDC hdc)
 {
 	switch (DirectKey) {
 	case VK_LEFT:
-		if (xDirect == 1)
+		if (xDirect == 1 || xDirect == -1)
 			break;
 		if (board[wormY[0]][wormX[0] - 1] != -1)
 		{
+			if (yDirect == 1) {
+				HeadBitmap = GetRotatedBitmap(hdc, HeadBitmap, -1 * M_PI / 2, RGB(0, 0, 0));
+				TailBitmap = GetRotatedBitmap(hdc, TailBitmap, -1 * M_PI / 2, RGB(0, 0, 0));
+			}
+			else {
+				HeadBitmap = GetRotatedBitmap(hdc, HeadBitmap, M_PI / 2, RGB(0, 0, 0));
+				TailBitmap = GetRotatedBitmap(hdc, TailBitmap, M_PI / 2, RGB(0, 0, 0));
+			}
 			xDirect = -1;
 			yDirect = 0;
 		}
 		break;
 	case VK_RIGHT:
-		if (xDirect == -1)
+		if (xDirect == -1 || xDirect == 1)
 			break;
 		if (board[wormY[0]][wormX[0] + 1] != -1)
 		{
+			if (yDirect == 1) {
+				HeadBitmap = GetRotatedBitmap(hdc, HeadBitmap, M_PI / 2, RGB(0, 0, 0));
+				TailBitmap = GetRotatedBitmap(hdc, TailBitmap, M_PI / 2, RGB(0, 0, 0));
+			}
+			else {
+				HeadBitmap = GetRotatedBitmap(hdc, HeadBitmap, -1 * M_PI / 2, RGB(0, 0, 0));
+				TailBitmap = GetRotatedBitmap(hdc, TailBitmap, -1 * M_PI / 2, RGB(0, 0, 0));
+			}
 			xDirect = 1;
 			yDirect = 0;
 		}
 		break;
 	case VK_UP:
-		if (yDirect == 1)
+		if (yDirect == 1 || yDirect == -1)
 			break;
-		if (board[wormY[0] - 1][wormX[0]] != -1)
+		if (board[wormY[0] - 1][wormX[0]] != -1 )
 		{
+			if (xDirect == 1) {
+				HeadBitmap = GetRotatedBitmap(hdc, HeadBitmap, M_PI / 2, RGB(0, 0, 0));
+				TailBitmap = GetRotatedBitmap(hdc, TailBitmap, M_PI / 2, RGB(0, 0, 0));
+			}
+			else {
+				HeadBitmap = GetRotatedBitmap(hdc, HeadBitmap, -1 * M_PI / 2, RGB(0, 0, 0));
+				TailBitmap = GetRotatedBitmap(hdc, TailBitmap, -1 * M_PI / 2, RGB(0, 0, 0));
+			}
 			xDirect = 0;
 			yDirect = -1;
 		}
 		break;
 	case VK_DOWN:
-		if (yDirect == -1)
+		if (yDirect == -1 || yDirect == 1)
 			break;
 		if (board[wormY[0] + 1][wormX[0]] != -1)
 		{
+			if (xDirect == 1) {
+				HeadBitmap = GetRotatedBitmap(hdc, HeadBitmap, -1 * M_PI / 2, RGB(0, 0, 0));
+				TailBitmap = GetRotatedBitmap(hdc, TailBitmap, -1 * M_PI / 2, RGB(0, 0, 0));
+			}
+			else {
+				HeadBitmap = GetRotatedBitmap(hdc, HeadBitmap, M_PI / 2, RGB(0, 0, 0));
+				TailBitmap = GetRotatedBitmap(hdc, TailBitmap, M_PI / 2, RGB(0, 0, 0));
+			}
 			xDirect = 0;
 			yDirect = 1;
 		}
@@ -285,18 +444,17 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT iMsg, WPARAM wParam, LPARAM lParam)
 {
 	HDC hdc;
 	PAINTSTRUCT ps;
+	hdc = BeginPaint(hwnd, &ps);
 	switch (iMsg) {
 	case WM_CREATE:
 		GameInit();
 		SetTimer(hwnd, 1, 100, NULL);
 		return 0;
 	case WM_PAINT:
-		hdc = BeginPaint(hwnd, &ps);
 		DrawGameBoard(hdc);
-		EndPaint(hwnd, &ps);
 		break;
 	case WM_KEYDOWN:
-		DirectControl((int)wParam);
+		DirectControl((int)wParam, hdc);
 		break;
 	case WM_TIMER:
 		MovingWorm(hwnd);
@@ -307,5 +465,6 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT iMsg, WPARAM wParam, LPARAM lParam)
 		PostQuitMessage(0);
 		return 0;
 	}
+	EndPaint(hwnd, &ps);
 	return(DefWindowProc(hwnd, iMsg, wParam, lParam));
 }
